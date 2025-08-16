e9a4436eb10ec0f9c802d3fb05f25ff06caa78f2
Raj Gupta <rajgupta2965@gmail.com>
2025-08-16 17:08:00 -0400

    Updated changes for a3

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -12,7 +12,7 @@ export default function TOC() {
       <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
           active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
-      <Nav.Item> <Nav.Link href="https://github.com/rajgupta2965/kambaz-react-web-app" target="_blank"> My GitHub </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link href="https://github.com/rajgupta2965/kambaz-react-web-app" id= "wd-github" target="_blank"> My GitHub </Nav.Link> </Nav.Item>
     </Nav>
   );
 }
